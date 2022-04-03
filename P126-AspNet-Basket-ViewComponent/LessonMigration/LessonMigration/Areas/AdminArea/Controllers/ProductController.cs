eb8a1fdb016d560aca146f087819e035f575a0f2
ibrahim <ibrahimea@code.edu.az>
2022-04-04 01:28:00 +0400

    Create

---
+++ P126-AspNet-Basket-ViewComponent/LessonMigration/LessonMigration/Areas/AdminArea/Controllers/ProductController.cs
@@ -0,0 +1,76 @@
+﻿using LessonMigration.Data;
+using LessonMigration.Models;
+using LessonMigration.Utilities.Pagination;
+using LessonMigration.ViewModels.Admin;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LessonMigration.Areas.AdminArea.Controllers
+{
+    [Area("AdminArea")]
+    public class ProductController : Controller
+    {
+        private readonly AppDbContext _context;
+        public ProductController(AppDbContext context)
+        {
+            _context = context;
+        }
+        public async Task <IActionResult> Index(int page = 1,int take=10)
+        {
+            var products = await _context.Products
+                .Include(m => m.Category)
+                .Include(m => m.Images)
+                .Skip((page - 1)*take)
+                .Take(take)
+                .AsNoTracking()
+                .OrderByDescending(m => m.Id)
+                .ToListAsync();
+
+            var productsVM  = GetMapDatas(products);
+
+            int count = await GetPageCount(take);
+
+            Paginate<ProductListVM> result = new Paginate<ProductListVM>(productsVM, page, count);
+            return View(result);
+        }
+
+        private async Task <int> GetPageCount(int take)
+        {
+            var count = await _context.Products.CountAsync();
+            return (int)Math.Ceiling((decimal)count / take);
+        }
+        private List <ProductListVM> GetMapDatas(List<Product>products)
+        {
+            List<ProductListVM> productList = new List<ProductListVM>();
+            foreach (var product in products)
+            {
+                ProductListVM newProduct = new ProductListVM
+                {
+                    Id = product.Id,
+                    Name = product.Name,
+                    Image = product.Images.Where(m => m.IsMain).FirstOrDefault()?.Image,
+                    CategoryName = product.Category.Name,
+                    Count = product.Count,
+                    Price=product.Price
+                };
+                productList.Add(newProduct);
+
+            }
+            return productList;
+
+        }
+
+        public async Task<IActionResult> Create()
+        {
+            var categories = await _context.Categories.Where(m => !m.IsDeleted).ToListAsync();
+            ViewBag.categories = new SelectList(categories, "Id", "Name");
+            return View();
+        }
+    }
+}
+    
---
+++ P126-AspNet-Basket-ViewComponent/LessonMigration/LessonMigration/ViewModels/Admin/ProductCreateVM.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LessonMigration.ViewModels.Admin
+{
+    public class ProductCreateVM
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public decimal Price { get; set; }
+        public int Count { get; set; }
+        public int CategoryId { get; set; }
+        public List<IFormFile> Photos { get; set; }
+    }
+}
